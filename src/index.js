2d15fbdca89bfe4d30de3c4b0e8fb117f1afb9bb
Ivan Berk <berkoivan@gmail.com>
2020-04-21 22:40:40 +0300

    added section search

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import "./sass/main.scss";
 import forecast from "./js/forecast";
+import search from "./js/search";
---
+++ src/js/search.js
@@ -0,0 +1,7 @@
+// const starBtn = document.querySelector(".favorites-btn");
+
+// starBtn.addEventListener("click", (e) => {
+//   console.dir(e.target);
+//   e.target.classList.remove("inactive-btn");
+//   e.target.classList.add("activ-bnt");
+// });
